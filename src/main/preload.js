b25bca1072e6b2b2e3951da0bb195c4f14b40f18
hejianxian <531601727@qq.com>
2021-01-26 22:49:39 +0800

    refactor: code beautify

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,145 +1,169 @@
-import { remote } from 'electron'
+import {
+  remote
+} from 'electron'
 import _ from 'lodash';
-const { ipcMain, dialog } = remote;
+const {
+  ipcMain,
+  dialog
+} = remote;
 
 const Cordova = require('./cordova').default
-const { contactWinShow, contactWinHide } = require('./ContactWindow')
+const {
+  contactWinShow,
+  contactWinHide
+} = require('./ContactWindow')
 export const getRole = () => {
-    const key = `role${remote.getCurrentWindow().id}`
-    const role = remote.getGlobal('shareRole')[key];
-    return role;
+  const key = `role${remote.getCurrentWindow().id}`
+  const role = remote.getGlobal('shareRole')[key];
+  return role;
 }
 export const getMainWin = () => remote.getGlobal('shareRole').mainwin
 export const getBus = () => remote.getGlobal('shareRole').bus;
 export const getContactWin = () => remote.getGlobal('shareRole').contactWin
 export const openContact = (type) => {
-    const mainwin = getMainWin();
-    mainwin.show();
-    mainwin.webContents.send('open-select-contact', remote.getCurrentWindow().id, type);
+  const mainwin = getMainWin();
+  mainwin.show();
+  mainwin.webContents.send('open-select-contact', remote.getCurrentWindow().id, type);
 }
 const key = `otherWindow${remote.getCurrentWindow().id}`;
 export const getOtherWin = () => remote.getGlobal('shareRole')[key];
 export const getImageShowWin = () => remote.getGlobal('shareRole').imageShowWindow;
 console.log('注入成功');
 const listenNavition = () => {
-    console.log('监听事件');
-    ipcMain.on('route-back', (event, arg) => {
-        console.log('监听事件 route-back');
-        window.history.go(-1)
-    })
-    ipcMain.on('close-devtools', (event, arg) => {
-        console.log('监听事件 close-devtools');
-        const otherWin = getOtherWin();
-        otherWin.webContents.closeDevTools();
-    })
-    ipcMain.on('open-devtools', (event, arg) => {
-        console.log('监听事件 open-devtools');
-        const otherWin = getOtherWin();
-        otherWin.webContents.openDevTools();
-    })
+  console.log('监听事件');
+  ipcMain.on('route-back', (event, arg) => {
+    console.log('监听事件 route-back');
+    window.history.go(-1)
+  })
+  ipcMain.on('close-devtools', (event, arg) => {
+    console.log('监听事件 close-devtools');
+    const otherWin = getOtherWin();
+    otherWin.webContents.closeDevTools();
+  })
+  ipcMain.on('open-devtools', (event, arg) => {
+    console.log('监听事件 open-devtools');
+    const otherWin = getOtherWin();
+    otherWin.webContents.openDevTools();
+  })
 }
 listenNavition();
 window.cordova = {
-   async exec(success, error, WorkPlusType, methodType, otherArgs) {
-        switch (methodType) {
-            case 'getUserTicket': {
-                const data = await Cordova.getUserTicket();
-                success(data);
-                break;
-            }
-            case 'getCurrentUserInfo': {
-                const data = await Cordova.getCurrentUserInfo();
-                success(data);
-                break;
-            }
-            case 'getLoginUserInfo': {
-                const data = await Cordova.getCurrentUserInfo();
-                success(data);
-                break; 
-            }
-            case 'getCurrentEmployeeInfo': {
-                const data = await Cordova.getCurrentEmployeeInfo();
-                success(data);
-                break;
-            }
-            case 'getContact':
-                const contactWin = getContactWin();
-                contactWin.show();
-                contactWin.webContents.closeDevTools();
-                contactWin.webContents.send('open-select-contact', remote.getCurrentWindow().id, 'contact');
-                ipcMain.on('render-reload', (event, arg) => {
-                    success(arg);
-                    contactWin.hide();
-                })
-                break;
-            case 'selectContacts':
-            case 'getEmployeesFromCurrentOrg':
-            case 'getContacts': {
-                const contactWin = getContactWin();
-                contactWin.show();
-                contactWin.webContents.closeDevTools();
-                contactWin.webContents.send('open-select-contact', remote.getCurrentWindow().id, 'contacts');
-                ipcMain.on('render-reload-getContacts', (event, arg) => {
-                    success(arg);
-                    contactWin.hide();
-                })
-                break;
-            }
-            case 'getWifiInfo': {
-                Cordova.getWifiInfo((data) => {
-                    success(data)
-                }, (err) => {
-                    error(err);
-                });
-                break;
-            }
+  async exec(success, error, WorkPlusType, methodType, otherArgs) {
+    switch (methodType) {
+      case 'getUserTicket': {
+        const data = await Cordova.getUserTicket();
+        success(data);
+        break;
+      }
+      case 'getCurrentUserInfo': {
+        const data = await Cordova.getCurrentUserInfo();
+        success(data);
+        break;
+      }
+      case 'getLoginUserInfo': {
+        const data = await Cordova.getCurrentUserInfo();
+        success(data);
+        break;
+      }
+      case 'getCurrentEmployeeInfo': {
+        const data = await Cordova.getCurrentEmployeeInfo();
+        success(data);
+        break;
+      }
+      case 'getContact':
+        const contactWin = getContactWin();
+        contactWin.show();
+        contactWin.webContents.closeDevTools();
+        contactWin.webContents.send('open-select-contact', remote.getCurrentWindow().id, 'contact');
+        ipcMain.on('render-reload', (event, arg) => {
+          success(arg);
+          contactWin.hide();
+        })
+        break;
+      case 'selectContacts':
+      case 'getEmployeesFromCurrentOrg':
+      case 'getContacts': {
+        const contactWin = getContactWin();
+        contactWin.show();
+        contactWin.webContents.closeDevTools();
+        contactWin.webContents.send('open-select-contact', remote.getCurrentWindow().id, 'contacts');
+        ipcMain.on('render-reload-getContacts', (event, arg) => {
+          success(arg);
+          contactWin.hide();
+        })
+        break;
+      }
+      case 'getWifiInfo': {
+        Cordova.getWifiInfo((data) => {
+          success(data)
+        }, (err) => {
+          error(err);
+        });
+        break;
+      }
 
-            case 'getIpAddress': {
-                const result = await Cordova.getIpAddress();
-                success(result);
-                break;
-            }
-            case 'getLocation': {
-                // const result = await Cordova.getLocation();
-                success('暂时不支持定位地址');
-                break;
-            }
-            case 'getDeviceInfo': {
-                Cordova.getDeviceInfo((data) => {
-                    success(data);
-                });
-                break;
-            }
-            case 'getAppInfo': {
-                const result = await Cordova.getAppInfo();
-                success(result);
-                break;
-            }
-            case 'selectImage': {
-                dialog.showOpenDialog({ properties: ['openFile'] }, { filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] },] })
-                  .then(result => {
-                    console.log(result);
-                    success({ path: _.get(result, `filePaths[0]`, '') })
-                })
-                break;
-            }
-            case 'showImages': {
-                dialog.showOpenDialog({ properties: ['multiSelections'] }, { filters: [{ name: 'Images', extensions: ['jpg', 'png', 'gif'] },] })
-                .then(result => {
-                  console.log(result);
-                  const imageShowWin = getImageShowWin();
-                  imageShowWin.show();
-                  imageShowWin.webContents.openDevTools();
-                  imageShowWin.webContents.send('image-show', _.get(result, `filePaths`, ''));
-                  ipcMain.on('render-image-show', (event, arg) => {
-                    imageShowWin.hide();
-                  })
-              })
-              break;
-            }
-            default: {
-                success('暂时不支持该查询');
-            }
-        }
+      case 'getIpAddress': {
+        const result = await Cordova.getIpAddress();
+        success(result);
+        break;
+      }
+      case 'getLocation': {
+        // const result = await Cordova.getLocation();
+        success('暂时不支持定位地址');
+        break;
+      }
+      case 'getDeviceInfo': {
+        Cordova.getDeviceInfo((data) => {
+          success(data);
+        });
+        break;
+      }
+      case 'getAppInfo': {
+        const result = await Cordova.getAppInfo();
+        success(result);
+        break;
+      }
+      case 'selectImage': {
+        dialog.showOpenDialog({
+            properties: ['openFile']
+          }, {
+            filters: [{
+              name: 'Images',
+              extensions: ['jpg', 'png', 'gif']
+            }, ]
+          })
+          .then(result => {
+            console.log(result);
+            success({
+              path: _.get(result, `filePaths[0]`, '')
+            })
+          })
+        break;
+      }
+      case 'showImages': {
+        dialog.showOpenDialog({
+            properties: ['multiSelections']
+          }, {
+            filters: [{
+              name: 'Images',
+              extensions: ['jpg', 'png', 'gif']
+            }, ]
+          })
+          .then(result => {
+            console.log(result);
+            const imageShowWin = getImageShowWin();
+            imageShowWin.show();
+            imageShowWin.webContents.openDevTools();
+            imageShowWin.webContents.send('image-show', _.get(result, `filePaths`, ''));
+            ipcMain.on('render-image-show', (event, arg) => {
+              imageShowWin.hide();
+            })
+          })
+        break;
+      }
+      default: {
+        success('暂时不支持该查询');
+      }
     }
+  }
 }
--- src/main/server/CordovaRequest.js
+++ src/main/server/CordovaRequest.js
@@ -1,36 +1,38 @@
 import _ from 'lodash';
 import BaseRequest from './BaseRequest.js';
 import Request from './Request.js';
-import { v4 as uuidv4 } from 'uuid';
+import {
+  v4 as uuidv4
+} from 'uuid';
 export default class CordovaRequest {
-    // static baseRequest = new BaseRequest();
-    static baseRequest = new Request();
+  // static baseRequest = new BaseRequest();
+  static baseRequest = new Request();
 
-    /** 获取token */
-    static getToken(obj) {
-        return new Promise((relove, reject) => {
-            const param = {
-              grant_type: 'password',
-              scope: 'user',
-              domain_id: obj.domain,
-              client_id: obj.user,
-              client_secret: obj.pwd,
-              client_secret_encrypt: false,
-              device_id: uuidv4(),
-              device_platform: 'PC'
-            };
-            const api = `${obj.api}/token`;
-            CordovaRequest.baseRequest.requestForPost(api, param).then(data => {
-                // console.log('getToken:',data);
-                const results = _.get(data, `data.result`, []);
-                relove(results);
-            }).catch(err => {
-              reject(err);
-            })
-          })
-    }
+  /** 获取token */
+  static getToken(obj) {
+    return new Promise((relove, reject) => {
+      const param = {
+        grant_type: 'password',
+        scope: 'user',
+        domain_id: obj.domain,
+        client_id: obj.user,
+        client_secret: obj.pwd,
+        client_secret_encrypt: false,
+        device_id: uuidv4(),
+        device_platform: 'PC'
+      };
+      const api = `${obj.api}/token`;
+      CordovaRequest.baseRequest.requestForPost(api, param).then(data => {
+        // console.log('getToken:',data);
+        const results = _.get(data, `data.result`, []);
+        relove(results);
+      }).catch(err => {
+        reject(err);
+      })
+    })
+  }
 
-    /**
+  /**
    * 获取组织架构
    */
   static getOrganization(token, api) {
@@ -46,7 +48,9 @@ export default class CordovaRequest {
    */
   static getUserTicket(token, api, orgId) {
     const url = `${api}/tickets?access_token=${token}`;
-    return CordovaRequest.baseRequest.requestForPost(url, { org_id: orgId }).then(data => {
+    return CordovaRequest.baseRequest.requestForPost(url, {
+      org_id: orgId
+    }).then(data => {
       const results = _.get(data, `data.result`, []);
       return results;
     })
